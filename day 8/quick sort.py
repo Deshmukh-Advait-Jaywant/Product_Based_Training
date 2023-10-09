105a1ead96be4785d625fcf3d9808068ca3fdfdc
Deshmukh Advait Jaywant <116196933+Deshmukh-Advait-Jaywant@users.noreply.github.com>
2023-10-09 08:57:35 +0530

    quick sort.py

--- day 8/quick sort.py
+++ day 8/quick sort.py
@@ -1,17 +1,12 @@
-def qsort(l,start,end):
-    pi=l[end]
-    i=start-1
-    for j in range(start,end):
-        if l[j]<pi:
-            i+=1
-            l[i],l[j]=l[j],l[i]
-    l[i+1],l[end]=l[end],l[i+1]
-    return i+1
-def quick(l,start,end):
-    if start<end:
-        pi=qsort(l,start,end)
-        quick(l,start,pi-1)
-        quick(l,pi+1,end)
-l=list(map(int,input().split()))
-quick(l,0,len(l)-1)
-print(l)
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+        
+    pivot = arr[0]
+    left = [i for i in arr if i < pivot]
+    right = [i for i in arr if i > pivot]
+    return quick_sort(left)+[pivot]+quick_sort(right)
+arr = [31,29,23,87,36,49]
+print(arr)
+result = quick_sort(arr)
+print(result)
